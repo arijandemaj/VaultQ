efe4df045f78468d2d12270a8a0c42e220922d69
Arijan Demaj <arijandemaj01@gmail.com>
2025-08-21 01:36:07 +0200

    Added Interfaces for services to easily do testing in the future

---
+++ VaultQ.Core/Interfaces/IEncryptionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultQ.Core.Interfaces
+{
+    public interface IEncryptionService
+    {
+        byte[] EncryptVault(byte[] vaultBytes, string password);
+        byte[] DecryptVault(byte[] vaultBytes, string password);
+
+    }
+}
