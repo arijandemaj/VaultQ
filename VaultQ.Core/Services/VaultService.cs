eee592022a1726b2c3b348b7ebb7a49e1942cdf1
Arijan Demaj <arijandemaj01@gmail.com>
2025-08-24 20:36:12 +0200

    Added SetupVault Method

--- VaultQ.Core/Services/VaultService.cs
+++ VaultQ.Core/Services/VaultService.cs
@@ -3,21 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaultQ.Core.Interfaces;
+using VaultQ.Core.Models;
 
 namespace VaultQ.Core.Services
 {
     public class VaultService
     {
-        public bool SetupVault(string vaultName, string vaultPassword)
+        private readonly IEncryptionService encryptionService;
+        private readonly IFileService fileService;
+
+        public VaultService(IEncryptionService encryptionService, IFileService fileService)
         {
-            // TODO:
-            // Derive the key from password
-            // Create in memory object 
-            // encrypt it with derived key
-            // Save to disk depending on the OS
-
-
-            return true;
+            this.encryptionService = encryptionService;
+            this.fileService = fileService;
         }
+
+        public static VaultService CreateDefault()
+        {
+            return new VaultService(new EncryptionService(), new FileService());
+        }
+
+        public void SetupVault(string vaultName, string vaultPassword)
+        {
+
+            try
+            {
+                var vaultCreation = new Vault
+                {
+                    Name = vaultName,
+                    Data = new Dictionary<string, string>()
+                };
+
+                byte[] serializedVault = fileService.SerializeVault(vaultCreation);
+                byte[] encryptedVault = encryptionService.EncryptVault(serializedVault, vaultPassword);
+
+                fileService.SaveToFile(encryptedVault, vaultName + ".dat");
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Something Went Wrong!");
+            }
+         
+        }
+
     }
 }
