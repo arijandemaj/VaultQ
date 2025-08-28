b2e8f7b27d348c34d77a4b3d81736475dd541952
Arijan Demaj <arijandemaj01@gmail.com>
2025-08-28 20:26:33 +0200

    Changed EncryptionService using helper methods

---
+++ VaultQ.Core/Constants/VaultHeaderSizes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultQ.Core.Enums
+{
+    internal class VaultHeaderSizes
+    {
+        public static int SaltSize 
+        {
+            get
+            {
+                return 16; // 128 bits Salt
+            }
+        }
+        public static int IVSize
+        {
+            get
+            {
+                return 15; // 128-bit IV for AES 
+            }
+        }
+
+        public static int HeaderSize
+        {
+            get
+            {
+                return SaltSize + IVSize;
+            }
+        }
+
+        public static int KeySize
+        {
+            get
+            {
+                return 32; // 256 bits key 
+            }
+        }
+
+        public static int IterationNumber
+        {
+            get
+            {
+                return 100_000;
+            }
+        }
+
+ 
+
+    }
+}
--- VaultQ.Core/Services/EncryptionService.cs
+++ VaultQ.Core/Services/EncryptionService.cs
@@ -5,6 +5,7 @@ using System.Net.NetworkInformation;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using VaultQ.Core.Enums;
 using VaultQ.Core.Interfaces;
 using VaultQ.Core.Models;
 
@@ -12,73 +13,80 @@ namespace VaultQ.Core.Services
 {
     internal class EncryptionService : IEncryptionService
     {
-        private const int saltSize = 16; // 128 bits Salt
-        private const int keySize = 32; // 256 bits key 
-        private const int ivSize = 16; // 128-bit IV for AES
-        private const int iterationNumber = 100_000; 
-       
         public byte[] EncryptVault(byte[] vaultBytes, char[] password)
         {
-            byte[] salt = RandomNumberGenerator.GetBytes(saltSize);
-            byte[] iv = RandomNumberGenerator.GetBytes(ivSize);
-            byte[] key = new byte[keySize];
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password, 0, password.Length);
+            byte[] salt = RandomNumberGenerator.GetBytes(VaultHeaderSizes.SaltSize);
+            byte[] iv = RandomNumberGenerator.GetBytes(VaultHeaderSizes.IVSize);
+            byte[] key = DeriveKeyFromPassword(password, salt, VaultHeaderSizes.IterationNumber);
+            byte[] encryptedData = Encrypt(vaultBytes, key, iv);
 
-            using (var pbkdf2 = new Rfc2898DeriveBytes(passwordBytes, salt, iterationNumber, HashAlgorithmName.SHA256))
-            {
-                key = pbkdf2.GetBytes(keySize);
-            }
+            byte[] result = new byte[VaultHeaderSizes.HeaderSize + encryptedData.Length];
+            Buffer.BlockCopy(salt, 0, result, 0, salt.Length);
+            Buffer.BlockCopy(iv, 0, result, salt.Length, iv.Length);
+            Buffer.BlockCopy(encryptedData, 0, result, VaultHeaderSizes.HeaderSize, encryptedData.Length);
 
-            Array.Clear(passwordBytes, 0, passwordBytes.Length);
 
-            using (Aes aes = Aes.Create())
-            {
-                ICryptoTransform encryptor = aes.CreateEncryptor(key, iv);
+            Array.Clear(salt, 0, salt.Length);
+            Array.Clear(iv, 0, iv.Length);
+            Array.Clear(key, 0, key.Length);
+            Array.Clear(encryptedData, 0, encryptedData.Length);
 
-                using(MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        cryptoStream.Write(vaultBytes, 0, vaultBytes.Length);
-                        cryptoStream.FlushFinalBlock();
-                    }
-
-                    byte[] encryptedData = ms.ToArray();
-                    byte[] result = new byte[saltSize + ivSize + encryptedData.Length];
-
-                    Buffer.BlockCopy(salt, 0, result, 0, saltSize);
-                    Buffer.BlockCopy(iv, 0, result, saltSize, ivSize);
-                    Buffer.BlockCopy(encryptedData, 0, result, saltSize + ivSize, encryptedData.Length);
-
-                    Array.Clear(salt, 0, saltSize);
-                    Array.Clear(iv, 0, ivSize);
-                    Array.Clear(key, 0, key.Length);
-
-                    return result;
-
-                }
-
-            }
+            return result;             
 
         }
         public byte[] DecryptVault(byte[] vaultBytes, char[] password)
         {
-            byte[] salt = vaultBytes.Take(saltSize).ToArray();
-            byte[] iv = vaultBytes.Skip(saltSize).Take(ivSize).ToArray();
-            byte[] data = vaultBytes.Skip(saltSize + ivSize).ToArray();
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password, 0, password.Length);
-            byte[] derivedKey;
-       
+            byte[] salt = vaultBytes.Take(VaultHeaderSizes.SaltSize).ToArray();
+            byte[] iv = vaultBytes.Skip(VaultHeaderSizes.SaltSize).Take(VaultHeaderSizes.IVSize).ToArray();
+
+            byte[] data = vaultBytes.Skip(VaultHeaderSizes.HeaderSize).ToArray();
+            byte[] derivedKey = DeriveKeyFromPassword(password, salt, VaultHeaderSizes.IterationNumber);
+            byte[] decryptedVault = Decrypt(data, derivedKey, iv);
+
+            Array.Clear(salt, 0, salt.Length);
+            Array.Clear(iv, 0, iv.Length);
+            Array.Clear(derivedKey, 0, derivedKey.Length);
+
+            return decryptedVault;
+        }
+
+        // Helper Method
+
+        private byte[] DeriveKeyFromPassword(char[] password, byte[] salt, int iterationNumber)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
             using (var pbkdf2 = new Rfc2898DeriveBytes(passwordBytes, salt, iterationNumber, HashAlgorithmName.SHA256))
             {
-                derivedKey = pbkdf2.GetBytes(keySize);
+                Array.Clear(passwordBytes, 0, passwordBytes.Length);
+                return pbkdf2.GetBytes(VaultHeaderSizes.KeySize);
             }
+            
+        }
 
-            Array.Clear(passwordBytes, 0, passwordBytes.Length);
-
+        private byte[] Encrypt(byte[] data, byte[] key, byte[] iv)
+        {
             using (Aes aes = Aes.Create())
             {
-                ICryptoTransform decryptor = aes.CreateDecryptor(derivedKey, iv);
+                ICryptoTransform encryptor = aes.CreateEncryptor(key, iv);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(data, 0, data.Length);
+                        cryptoStream.FlushFinalBlock();
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        private byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                ICryptoTransform decryptor = aes.CreateDecryptor(key, iv);
 
                 using (MemoryStream ms = new MemoryStream())
                 {
@@ -88,18 +96,10 @@ namespace VaultQ.Core.Services
                         cryptoStream.FlushFinalBlock();
                     }
 
-                    byte[] decryptedData = ms.ToArray();
-
-                    Array.Clear(salt, 0, saltSize);
-                    Array.Clear(iv, 0, ivSize);
-                    Array.Clear(derivedKey, 0, derivedKey.Length);
-
-                    return decryptedData;
-
+                    return ms.ToArray();
                 }
 
             }
-
         }
 
 
